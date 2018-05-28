6aa9d825b333c6381fd83c4d531063fc480462f9
james <12348978@qq.com>
2018-05-28 15:13:09 +0800

    test

---
+++ comm_apps/mj_communication.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018-05-28 15:11
+# @Author  : James
+# @Site    : 
+# @File    : mj_communication.py
+# @Software: PyCharm
--- sdtimj/settings.py
+++ sdtimj/settings.py
@@ -18,6 +18,8 @@ import sys
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 sys.path.insert(0, os.path.join(BASE_DIR, 'extra_apps'))
+sys.path.insert(0, os.path.join(BASE_DIR, 'comm_apps'))
+
 
 
 # Quick-start development settings - unsuitable for production
