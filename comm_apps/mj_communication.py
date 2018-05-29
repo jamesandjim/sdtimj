fd01199a4ea29b91be6be4756b3a9eee37c8ed95
james <12348978@qq.com>
2018-05-29 13:40:42 +0800

    test

--- comm_apps/mj_communication.py
+++ comm_apps/mj_communication.py
@@ -5,3 +5,37 @@
 # @Site    : 
 # @File    : mj_communication.py
 # @Software: PyCharm
+from socket import *
+
+
+class WGPacketShort():
+    global WGPacketSize, Type, ControllerPort, SpecialFlag, functionID, buff, iDevSn, data
+    WGPacketSize = 64
+    Type = 0x17
+    ControllerPort = 60000
+    SpecialFlag = 0x55AAAA55
+    functionID = 0xff
+    buff = []
+    iDevSn = 0
+    data = [56]
+
+    def to_byte(self, buflen):
+        if buflen == WGPacketSize:
+            buff[0] = Type
+            buff[1] = functionID
+            buff[4:8] = iDevSn
+            buff[8:65] = data
+
+            return buff
+
+    def run(self, ip, port):
+        s = socket(AF_INET, SOCK_DGRAM)
+        s.bind(ip, port)
+
+        s.sendto(buff)
+
+        
+
+
+
+
