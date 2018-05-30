d6c9815fd9cca5fd9b4c5956bc6119ba30aa794f
james <12348978@qq.com>
2018-05-30 14:53:22 +0800

    20180530

--- comm_apps/mj_communication.py
+++ comm_apps/mj_communication.py
@@ -7,9 +7,13 @@
 # @Software: PyCharm
 from socket import *
 
+#数据报及其操作方法
+#4、接收服务器发送回的数据包
+#5、对接收到的数据包进行解析
 
 class WGPacketShort():
-    global WGPacketSize, Type, ControllerPort, SpecialFlag, functionID, buff, iDevSn, data
+
+    global WGPacketSize, Type, ControllerPort, SpecialFlag, functionID, buff, iDevSn, data, ADDR #1、定义需发送的数据包的内容
     WGPacketSize = 64
     Type = 0x17
     ControllerPort = 60000
@@ -18,8 +22,11 @@ class WGPacketShort():
     buff = []
     iDevSn = 0
     data = [56]
+    HOST = '192.168.0.100'
+    PORT = 60000
+    ADDR = (HOST, PORT)
 
-    def to_byte(self, buflen):
+    def to_byte(self, str, buflen):                                                         #2、对需发送的内容进行格式化
         if buflen == WGPacketSize:
             buff[0] = Type
             buff[1] = functionID
@@ -28,11 +35,34 @@ class WGPacketShort():
 
             return buff
 
-    def run(self, ip, port):
-        s = socket(AF_INET, SOCK_DGRAM)
-        s.bind(ip, port)
+    def run(self, buff, ADDR):                                                                #3、基于UDP协议发送数据包
+        udp_cli_socket = socket(AF_INET, SOCK_DGRAM)
 
-        s.sendto(buff)
+        while True:
+            try:
+                udp_cli_socket.sendto(buff, ADDR)
+                data, ADDR = udp_cli_socket.recvfrom(64)
+
+                if not data:
+                    break
+                if len(data) == WGPacketSize:
+                    if data[0] == Type and data[1] == functionID:
+                        return data
+
+            except Exception,  e:
+                pass
+
+        udp_cli_socket.close()
+
+
+#对收到的数据进行解析
+   
+
+
+
+
+
+
 
 
 
